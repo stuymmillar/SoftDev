c94b96c1165effc196f8fd9466eafc11dc780cdb
Max Millar <mmillar@stuy.edu>
2018-09-20 23:12:26 -0400

    added back 3routes

---
+++ 08_lemme_flask_u_sumpn/3routes.py
@@ -0,0 +1,23 @@
+from flask import Flask
+app= Flask(__name__) #create isntanc of class Flask
+
+@app.route("/") #assign fxn to route
+def hello_world():
+    print(__name__)
+    return "<h1><i><center><a href'/page2'> No hablo queso! </a></center></i></h1>"
+
+@app.route("/page2")
+def also_hello_world():
+	print(__name__)
+	return "<a href='page3'>i like to call this one, page 2!</a>"
+
+
+@app.route("/page3")
+def also_hello_world():
+	print(__name__)
+	return "<a href='/'>and this one? page 3!</a>"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
