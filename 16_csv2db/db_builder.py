717741fd782aa1953f8136da77e5fba661fa3929
stuymmillar <mmillar@stuy.edu>
2018-10-04 21:19:34 -0400

    added comments to db_builder

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -13,33 +13,30 @@ db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
 def makeTable(filename):
-    with open(filename, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
+    with open(filename, 'r') as csvfile: # open csv file
+        reader = csv.DictReader(csvfile) #read it in as a set of dictionaries
         #print(reader.get(0))
-        first = True
+        first = True #to only perform this on the first iteration
         for row in reader:
             if first:
-                command = "CREATE TABLE " + filename[:len(filename) - 4] + " ("
+                command = "CREATE TABLE " + filename[:len(filename) - 4] + " (" #initialize name of table and beginning of creation statement
                 for column in row.keys(): 
-                    command += "'" + column + "' BLOB, "
+                    command += "'" + column + "' BLOB, " #add each column in generically typed
                     #print(command[:len(command) - 2] + ")")
-                c.execute(command[:len(command) - 2] + ")")
-                db.commit
-                first = False
-            command2 = "INSERT INTO " + filename[:len(filename) - 4] + " VALUES ("
+                c.execute(command[:len(command) - 2] + ")") #execute commmand
+                first = False #make sure this section does not run again
+            command2 = "INSERT INTO " + filename[:len(filename) - 4] + " VALUES (" #initialize insert statement with table name
             for coldata in row.keys():
-                command2 += "'" + row[coldata] + "', "
-            c.execute(command2[:len(command2) - 2] + ")")
-            db.commit()
-        #for row in reader:
-            #print(row)
+                command2 += "'" + row[coldata] + "', " #add each column of data to statement
+            c.execute(command2[:len(command2) - 2] + ")") #execute command
 
 makeTable("peeps.csv")
-makeTable("courses.csv")
+makeTable("courses.csv") #run for both csv files
 c.execute("SELECT * FROM peeps")
 print(c.fetchall())
 c.execute("SELECT * FROM courses")
-print(c.fetchall())
+print(c.fetchall()) #print data from each table
+
 
 db.commit() #save changes
 db.close()  #close database
