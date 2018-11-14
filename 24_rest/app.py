532ff740705ab089beafc313729f04f438e5e17d
Max Millar <33496245+stuymmillar@users.noreply.github.com>
2018-11-13 22:48:52 -0500

    Update app.py

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_test():
-    data = json.loads((urllib.request.urlopen("https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=h9chfP0S0Fxq5oKty7ve1o08M6EhA6dVRtkvroWK")).read().decode())
+    data = json.loads((urllib.request.urlopen("https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=h9chfP0S0Fxq5oKty7ve1o08M6EhA6dVRtkvroWK")).read())
     return render_template("index.html", url=data["url"])
 
 if __name__ == '__main__':
