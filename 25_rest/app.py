5a827b09f891b4dcee0d225d37c8e28cec1eaf62
Max <mmillar@stuy.edu>
2018-11-14 22:51:21 -0500

    finished k25

--- 25_rest/app.py
+++ 25_rest/app.py
@@ -1,3 +1,8 @@
+#Max Millar
+#SoftDev1 pd06
+#k25 -- Getting More REST
+#2018-11-14
+
 from flask import Flask, render_template
 import json
 from urllib import request
@@ -6,8 +11,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_test():
-    data = json.loads((urllib.request.urlopen("https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=h9chfP0S0Fxq5oKty7ve1o08M6EhA6dVRtkvroWK")).read())
-    return render_template("index.html", url=data["url"])
+    data = json.loads((request.urlopen("https://en.wikipedia.org/w/api.php?action=parse&page=Barack_Obama&format=json")).read())
+    print(data)
+    print("==================================================================")
+    print(data["parse"]["text"]["*"])
+    return render_template("index.html", text=data["parse"]["text"]["*"], title=data["parse"]["title"])
 
 if __name__ == '__main__':
     app.debug = True
